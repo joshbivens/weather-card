7e4caaf75ce9f500c6485f568719ba8732f66a45
Josh Bivens <hotbioform@gmail.com>
2015-11-13 10:41:30 -0700

    Utilize weather api

--- src/js/weather-info.js
+++ src/js/weather-info.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
-	$(".temp").html(30 + "&#176");
+	var zip = "87122,us"
+	var key = "52965110eb4daf6fc4842ebf4e9207aa";
+	var uri = "http://api.openweathermap.org/data/2.5/weather?q=" + zip + "&units=imperial&APPID=" + key;
+	
+	
+	$.get(uri, function(r) {
+		var currentTemp = Math.floor(r.main.temp);
+		var currentWeather = r.weather[0].id;
+		
+		$(".temp").html(currentTemp + "&#176");
+		
+		if (currentWeather === 800) {
+			$(".icon").append("<i class='fa fa-sun-o fa-4x'></i>");
+		} else if (currentWeather >= 500 && currentWeather <= 531) {
+			$(".icon").append("<i class='fa fa-tint fa-4x'></i>");
+		} else if (currentWeather >= 801 && currentWeather <= 804) {
+			$(".icon").append("<i class='fa fa-cloud fa-4x'></i>");
+		} else {
+			$(".icon").append("<i class='fa fa-coffee fa-4x'></i>");
+		}
+		
+		console.log(currentWeather);
+		
+	}, "json");
 });
+
+//var $temp = $(".temp").html(temp + "&#176");
